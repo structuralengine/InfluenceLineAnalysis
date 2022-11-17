a03fad22ab6b0ded5a09859a1e14cd186e24cf4e
sasaco <sasaicco@gmail.com>
2022-11-17 09:47:19 +0900

    チェレンジ用

--- src/dataInput.py
+++ src/dataInput.py
@@ -12,23 +12,8 @@ class dataInput:
     def __init__(self):
         self.isCompress = False
 
-    def setCompressJSON(self, data):
-        self.isCompress = True
-        js = self.decompress(data)
-        self.setJSONString(js)
-
     def setJSONString(self, fstr):
         js = json.loads(fstr, object_pairs_hook=dict)
-        self.setJSON(js)
-
-    def setJSON(self, js: dict):
-
-        self.production = js['production'] if 'production' in js else True
-        self.uid = js['uid'] if 'uid' in js else None
-        self.username = js['username'] if 'username' in js else ''
-        self.password = js['password'] if 'password' in js else ''
-
-        # ケースによって変わらないパラメータを取得
 
         ## 節点
         self.node = OrderedDict(js['node'])
@@ -42,63 +27,16 @@ class dataInput:
                     n[key] = 0.0
 
 
-        ## 部材
-        self.member = []
-        if 'member' in js:
-            self.member = js['member']
-            # 型をstring に 統一する, 部材長さを計算しておく
-            for ID in self.member:
-                m = self.member[ID]
-                m['ni'] = str(m['ni'])
-                m['nj'] = str(m['nj'])
-                m['L'] =  round(self.GetLength(m) * 1000) #部材長さを計算しておく
-                # 節点に関する整合性をチェック
-                for n in [m['ni'], m['nj'] ]:
-                    # 存在しない節点を含むシェルはエラー
-                    if not n in self.node:
-                        return 'have a shell that contains nodes that do not exist.'
-
-
         ## 解析結果
         self.dict_fsec = []
         if 'result' in js:
             self.dict_fsec = js['result']
-            # 型をstring に 統一する
 
 
+        ## 荷重
+        self.load = []
+        if 'load' in js:
+            self.load = js['load']
 
 
         return None
-
-    # 部材の長さを計算する
-    def GetLength(self, target: dict):
-        IDi   = target['ni']
-        IDj   = target['nj']
-        pi = self.node[IDi]
-        pj = self.node[IDj]
-        return self.GetDistance(pi, pj)
-
-    # 2点間の距離を計算する
-    def GetDistance(self, pi: dict, pj: dict):
-        xx = pj['x']-pi['x']
-        yy = pj['y']-pi['y']
-        zz = pj['z']-pi['z']
-        return np.sqrt(xx**2+yy**2+zz**2)
-
-    def compress(self, js: str):
-        # gzip圧縮する
-        l = gzip.compress(js.encode())
-        # Base64エンコードする
-        byteBase64 = base64.b64encode(l)
-        # string に変換
-        return byteBase64.decode()
-
-    def decompress(self, byteBase64):
-        # base64型 を もとに戻す
-        b = base64.b64decode(byteBase64)
-        # str型に変換し、カンマでばらして int 配列に変換する
-        l = eval(b) #[int(n) for n in b.decode().split(',')]
-        # gzipを解凍する
-        return gzip.decompress(bytes(l))
-
-
--- tests/test_json.py
+++ tests/test_json.py
@@ -4,51 +4,28 @@ import json
 
 def test_001():
 
-    # from src.calcrate import FrameCalc
     from src.dataInput import dataInput
-    # from src.usage import usage
+    from src.calcrate import FrameCalc
 
 
     # 入力データを読み込む
     folder_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
-    int_file_path = os.path.join(folder_path, 'example_files/test.json')
+    int_file_path = os.path.join(folder_path, 'example_files/min_test.json')
     f = open(int_file_path, encoding="utf-8")
     fstr = f.read()  # ファイル終端まで全て読んだデータを返す
     f.close()
     inp = dataInput()
     inp.setJSONString(fstr)
 
-    #calc = FrameCalc(inp)
-
-    # ユーザーチェック
-    # user = usage(inp.production)
-    # error = user.checkUser(inp)
-    # if error != None:
-    #     test_out_json(json.dumps({'error': error['message']}))
-
-
-    # 計算開始
-    #error, result = calc.calcrate()
-    # if error != None:
-    #     test_out_json(json.dumps({'error': error}))
-
-    # # 使用量を計算する
-    # for key in result:
-    #     re = result[key]
-    #     user.deduct_points += re['size']
-
-    # result['old_points'] = user.old_points
-    # result['deduct_points'] = user.deduct_points
-    # result['new_points'] = user.writeFirestore()
-
+    # 計算する
+    out_text = FrameCalc(inp).calcrate()
 
     # 結果を返送する
-    # out_text = json.dumps(result)
-    out_file_path = os.path.join(folder_path, 'example_files/test.out.json')
-    # fout=open(out_file_path, 'w')
-    # print(out_text, file=fout)
-    # fout.close()
-    # exit()
+    out_file_path = os.path.join(folder_path, 'example_files/min_test.out.json')
+    fout=open(out_file_path, 'w')
+    print(out_text, file=fout)
+    fout.close()
+    exit()
 
 
 if __name__ == "__main__": 
